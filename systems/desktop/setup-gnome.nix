dfa19812d3573de5ef447ecf08ac904605ee3276
tiramission <37465086+tiramission@users.noreply.github.com>
2025-01-14 15:16:55 +0000

    split desktop config

---
+++ systems/desktop/setup-gnome.nix
@@ -0,0 +1,27 @@
+{pkgs, ...}: {
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
+  environment.gnome.excludePackages = with pkgs; [
+    atomix # puzzle game
+    cheese # webcam tool
+    epiphany # web browser
+    evince # document viewer
+    geary # email reader
+    gedit # text editor
+    gnome-characters
+    gnome-music
+    gnome-photos
+    gnome-terminal
+    gnome-tour
+    hitori # sudoku game
+    iagno # go game
+    tali # poker game
+    totem # video player
+  ];
+
+  users.users.gdm = {extraGroups = ["video"];};
+  users.users.jaign = {extraGroups = ["video"];};
+}
--- systems/setup-desktop.nix
+++ systems/setup-desktop.nix
@@ -1,33 +1,7 @@
-{pkgs, ...}: {
-  services.xserver = {
-    enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
-  };
-  environment.gnome.excludePackages = with pkgs; [
-    atomix # puzzle game
-    cheese # webcam tool
-    epiphany # web browser
-    evince # document viewer
-    geary # email reader
-    gedit # text editor
-    gnome-characters
-    gnome-music
-    gnome-photos
-    gnome-terminal
-    gnome-tour
-    hitori # sudoku game
-    iagno # go game
-    tali # poker game
-    totem # video player
+{...}: {
+  imports = [
+    ./desktop/setup-gnome.nix
+    ./desktop/setup-xrdp.nix
+    ./desktop/setup-ime.nix
   ];
-
-  users.users.gdm = {extraGroups = ["video"];};
-  users.users.jaign = {extraGroups = ["video"];};
-
-  i18n.inputMethod = {
-    enable = true;
-    type = "ibus";
-    ibus.engines = with pkgs.ibus-engines; [libpinyin];
-  };
 }
