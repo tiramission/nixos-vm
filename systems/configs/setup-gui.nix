8cbc482a90d86186baa6b59281005ca7cf75f0c5
tiramission <37465086+tiramission@users.noreply.github.com>
2025-05-20 11:00:18 +0000

    use gnome remote replace xrdp

--- systems/configs/setup-gui.nix
+++ systems/configs/setup-gui.nix
@@ -1,7 +1,7 @@
 {...}: {
   imports = [
     ../desktop/setup-gnome.nix
-    ../desktop/setup-xrdp.nix
+    # ../desktop/setup-xrdp.nix (use gnome remote)
     ../desktop/setup-ime.nix
     ../desktop/setup-font.nix
   ];
--- systems/desktop/setup-gnome.nix
+++ systems/desktop/setup-gnome.nix
@@ -8,23 +8,12 @@
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
-  environment.gnome.excludePackages = with pkgs; [
-    atomix # puzzle game
-    cheese # webcam tool
-    epiphany # web browser
-    evince # document viewer
-    geary # email reader
-    gedit # text editor
-    gnome-characters
-    gnome-music
-    gnome-photos
-    gnome-terminal
-    gnome-tour
-    hitori # sudoku game
-    iagno # go game
-    tali # poker game
-    totem # video player
-  ];
+  systemd.services.gnome-remote-desktop = {
+    wantedBy = ["graphical.target"];
+  };
+
+  networking.firewall.allowedTCPPorts = [3389];
+  networking.firewall.allowedUDPPorts = [3389];
 
   users.users.gdm = {extraGroups = ["video"];};
   users.users.${params.username} = {extraGroups = ["video"];};
