4cefb689074410ff519cee33e2c819203190bb46
tiramission <37465086+tiramission@users.noreply.github.com>
2025-10-09 03:38:44 +0000

    zsh: change theme

--- homes/setups/apps/zsh.nix
+++ homes/setups/apps/zsh.nix
@@ -1,14 +1,27 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  src = pkgs.fetchFromGitHub {
+    owner = "JannoTjarks";
+    repo = "catppuccin-zsh";
+    rev = "main";
+    sha256 = "sha256-5mivlJpfp89VNMg9PS20DaMYnws+62Yrktlx5On2Puw=";
+  };
+in {
   programs.zsh = {
     enable = true;
     autocd = true;
     autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
     oh-my-zsh = {
       enable = true;
-      theme = "robbyrussell";
+      custom = "${src}";
+      theme = "catppuccin";
       plugins = [
         "git"
       ];
+      extraConfig = ''
+        CATPPUCCIN_FLAVOR="macchiato"
+        CATPPUCCIN_SHOW_TIME=true
+      '';
     };
     initContent = ''
       if [[ -f ~/.env ]]; then
