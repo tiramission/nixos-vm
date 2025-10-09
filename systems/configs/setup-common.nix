1bc6ac511e94123aa77fee715a19953b5ed7855c
tiramission <37465086+tiramission@users.noreply.github.com>
2025-10-09 15:34:44 +0800

    set timezone

--- systems/configs/setup-common.nix
+++ systems/configs/setup-common.nix
@@ -37,6 +37,7 @@ in {
   security.sudo.wheelNeedsPassword = false;
 
   i18n.defaultLocale = "zh_CN.UTF-8";
+  time.timeZone = "Asia/Shanghai";
 
   system.stateVersion = "25.05"; # Did you read the comment?
   nixpkgs.config.allowUnfree = true;
